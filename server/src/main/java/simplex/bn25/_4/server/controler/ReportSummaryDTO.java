06b4665916e2fabf4f8abe5ca5784193c78a6d5e
nakagawaryogo <nakaryou1214@gamil.com>
2025-05-26 03:24:07 +0900

    とりあえずの振り返り質向上の機能追加（フィードバック忘れててる）

---
+++ server/src/main/java/simplex/bn25/_4/server/controler/ReportSummaryDTO.java
@@ -0,0 +1,10 @@
+package simplex.bn25._4.server.controler;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+
+public record ReportSummaryDTO(
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate reportDate,
+        String t
+) {}
